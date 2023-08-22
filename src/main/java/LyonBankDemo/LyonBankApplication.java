6b61a3ab695b0cd082d23f8f430e8d60e543be7f
KD08-DEV <kevinjosexd@gmail.com>
2023-08-22 00:56:05 -0400

    Fixed Dockerfile

--- src/main/java/LyonBankDemo/LyonBankApplication.java
+++ src/main/java/LyonBankDemo/LyonBankApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LyonBankApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LyonBankApplication.class, args);
 	}
